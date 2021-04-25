cff853387c2641ef52ff8e9ad8df1ed95b052584
Kostia <kayarmolenka@gmail.com>
2021-04-25 22:49:34 +0300

    add store

--- src/components/state.js
+++ src/components/state.js
@@ -1,43 +1,45 @@
-let renderPage = () => {
-	console.log('sad')
+let store = {
+	_state: {
+		persons: [
+			{id: 1, name:'Nastya', avatar: '/img/nastya.jpg'},
+			{id: 2, name:'Koly', avatar: '/img/no_avatar.png'},
+			{id: 3, name:'Maxim', avatar: '/img/no_avatar.png'},
+			{id: 4, name:'Nikita', avatar: '/img/no_avatar.png'},
+			{id: 5, name:'Alina', avatar: '/img/no_avatar.png'},
+			{id: 6, name:'Anna', avatar: '/img/no_avatar.png'}
+		],
+		dialogs: [
+			{id: 1, message: `What's your name?`, avatar: '/img/nastya.jpg'}
+		],
+		posts: [
+			{id: 1, message: 'Bring coffee mother and father', likesCount: 3},
+			{id: 2, message: 'I want to speak English very well, correctly and fluently', likesCount: 3},
+		],
+		newPostText: ''
+	},
+	getState() {
+		return this._state;
+	},
+	_renderPage() {
+		console.log('sad')
+	},
+	addPost() {
+		const newPost = {
+			id: 3, 
+			message: this._state.newPostText,
+			likesCount: 0
+		};
+		this._state.posts.push(newPost);
+		this._state.newPostText = '';
+		this._renderPage(this._state);
+	},
+	updateTextPost(newText) {
+		this._state.newPostText = newText;
+		this._renderPage(this._state);
+	},
+	subscribe(observer) {
+		this._renderPage = observer;
+	},
 }
 
-const state = {
-	persons: [
-		{id: 1, name:'Nastya', avatar: '/img/nastya.jpg'},
-		{id: 2, name:'Koly', avatar: '/img/no_avatar.png'},
-		{id: 3, name:'Maxim', avatar: '/img/no_avatar.png'},
-		{id: 4, name:'Nikita', avatar: '/img/no_avatar.png'},
-		{id: 5, name:'Alina', avatar: '/img/no_avatar.png'},
-		{id: 6, name:'Anna', avatar: '/img/no_avatar.png'}
-	],
-	dialogs: [
-		{id: 1, message: `What's your name?`, avatar: '/img/nastya.jpg'}
-	],
-	posts: [
-		{id: 1, message: 'Bring coffee mother and father', likesCount: 3},
-		{id: 2, message: 'I want to speak English very well, correctly and fluently', likesCount: 3},
-	],
-	newPostText: ''
-}
-
-export const addPost = () => {
-	const newPost = {
-		id: 3, 
-		message: state.newPostText,
-		likesCount: 0
-	};
-	state.posts.push(newPost);
-	state.newPostText = '';
-	renderPage(state);
-}
-
-export const updateTextPost = (newText) => {
-	state.newPostText = newText;
-	renderPage(state);
-}
-
-export const subscribe = (observer) => {
-	renderPage = observer;
-}
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import state, {subscribe} from './components/state';
+import store  from './components/state';
 import './index.css';
 import App from './components/App/App';
 import {BrowserRouter} from 'react-router-dom';
-import {addPost, updateTextPost} from './components/state';
 
 const renderPage = (state) => {
 	ReactDOM.render(
 		<BrowserRouter>
-			<App state={state} addPost={addPost} updateTextPost={updateTextPost}/>
+			<App state={store.getState()} addPost={store.addPost.bind(store)} updateTextPost={store.updateTextPost.bind(store)}/>
 		</BrowserRouter>,
 		document.getElementById('root')
 	);
 }
 
-renderPage(state);
+renderPage(store.getState());
 
-subscribe(renderPage);
+store.subscribe(renderPage);
